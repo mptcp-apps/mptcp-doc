6eaaa9f905d24ec8e69cb6838ffc578c32be186d
Olivier Bonaventure <Olivier.Bonaventure@uclouvain.be>
2022-11-07 14:44:08 +0100

    Logo and icon

--- conf.py
+++ conf.py
@@ -178,7 +178,12 @@ html_static_path = ['_static']
 #
 # html_sidebars = {}
 
+html_logo = "_static/mptcp-logo.png"
 
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = "_static/favicon.ico"
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
